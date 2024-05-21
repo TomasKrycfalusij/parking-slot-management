2956e6225d062bbfb56adbbc78f5ed3e213d0822
Tomáš Kryčfalušij <nestrog26@gmail.com>
2024-05-21 12:31:03 +0200

    docker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
   return (
     <>
       <Head>
@@ -16,6 +18,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <h1>Registrace</h1>
+
       </main>
     </>
   );
