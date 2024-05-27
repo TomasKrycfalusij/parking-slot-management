14d8667cf46ef7f7e21b6035f1b4192357e2ba67
Tomáš Kryčfalušij <nestrog26@gmail.com>
2024-05-27 11:03:44 +0200

    test databaze

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,6 +10,22 @@ const page = async () => {
   
   return (
     <div className={pageStyle.main}>
+      {
+        days?.map((day) => {
+          return (
+            <div key={day.id}>
+              <h2>{day.date.toString()}</h2>
+              <div>{day.bookings.map((reservation) => {
+                return (
+                  <p key={reservation}>{reservation}</p>
+                )
+              })}
+              </div>
+            </div>
+          )
+        
+        })
+      }
       <CalendarRegistration days={days}/>
       <LogOut />
     </div>
