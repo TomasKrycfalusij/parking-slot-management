930d7e4399bc7707e846ab5bcce5747ec2b0d32b
Tomáš Kryčfalušij <nestrog26@gmail.com>
2024-05-28 10:59:57 +0200

    další styly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,34 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import navbarStyles from './navbarStyles.module.css';
 import { useUser } from './UserContext';
 
 const Navbar = () => {
     const { logOutUser } = useUser();
-  return (
-    <div className={navbarStyles.navContainer}>
-      <p>BATMAN</p>
-      <ul className={navbarStyles.horizontalList}>
+    const [navVisible, setNavVisible] = useState(false);
+
+    const toggleNav = () => {
+      setNavVisible(prev => !prev);
+    }
+
+    return (
+      <>
+        <div className={navbarStyles.navContainer}>
+            <p>BATMAN</p>
+                <button onClick={() => toggleNav()} className={navbarStyles.navButton}>MENU</button>
+                <ul className={`${navbarStyles.horizontalList} ${navVisible ? navbarStyles.navActive : ''} ${navbarStyles.navUlPc}`}>
+                    <li><a href="/calendar">Kalendář</a></li>
+                    <li><a href="/registrations">Registrace</a></li>
+                    <li><a onClick={() => logOutUser()}>Odhlásit se</a></li>
+                </ul>
+        </div>
+      <ul className={`${navbarStyles.horizontalList} ${navVisible ? navbarStyles.navActive : ''} ${navbarStyles.navUlPhone}`}>
         <li><a href="/calendar">Kalendář</a></li>
         <li><a href="/registrations">Registrace</a></li>
         <li><a onClick={() => logOutUser()}>Odhlásit se</a></li>
       </ul>
-    </div>
-  );
+    </>
+    );
 };
 
 export default Navbar;
